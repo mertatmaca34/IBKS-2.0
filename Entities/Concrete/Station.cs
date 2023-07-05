d718b36cfa6d2b32a05d5e2b70d17de69755e1bd
Mert Atmaca <mertatmaca34@gmail.com>
2023-07-05 11:45:22 +0300

    ++

--- Entities/Concrete/Station.cs
+++ Entities/Concrete/Station.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    internal class Station
+    public class Station : IEntity
     {
+        public int Id { get; set; }
+        public required string StationName { get; set; }
+        public Guid StationId { get; set; }
     }
 }
