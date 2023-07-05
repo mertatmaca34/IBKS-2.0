617ed8ea3b2d009dc07efc98d941991f946884ed
Mert Atmaca <mertatmaca34@gmail.com>
2023-07-05 11:57:25 +0300

    ++

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
+    }
+}
