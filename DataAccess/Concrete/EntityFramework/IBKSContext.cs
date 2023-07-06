37dad8a11e70664323f689cc7371c22c86461524
Mert Atmaca <mertatmaca34@gmail.com>
2023-07-06 10:06:07 +0300

    ++

--- DataAccess/Concrete/EntityFramework/IBKSContext.cs
+++ DataAccess/Concrete/EntityFramework/IBKSContext.cs
@@ -1,10 +1,5 @@
 ﻿using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
