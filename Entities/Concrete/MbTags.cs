329cc870c99fde2686dc640975a86c40e7244cd4
Mert Atmaca <mertatmaca34@gmail.com>
2023-07-12 13:06:29 +0300

    ++

---
+++ Entities/Concrete/MbTags.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class MbTags : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime ReadTime { get; set; }
+        public bool YikamaVarMi { get; set; }
+        public bool HaftalikYikamaVarMi { get; set; }
+        public bool ModAutoMu { get; set; }
+        public bool ModBakimMi { get; set; }
+        public bool ModKalibrasyonMu { get; set; }
+        public bool AkmTetik { get; set; }
+        public bool KoiTetik { get; set; }
+        public bool PhTetik { get; set; }
+    }
+}
--- PLC/Sharp7/Concrete/PlcService.cs
+++ PLC/Sharp7/Concrete/PlcService.cs
@@ -10,10 +10,11 @@ namespace PLC.Sharp7.Concrete
         private static PlcService instance = null;
 
         public S7Client client = new();
+
         public DB41 DB41 = new();
+        private readonly byte[] buffer41 = new byte[248];
 
         private readonly BackgroundWorker _worker = new();
-        private readonly byte[] buffer41 = new byte[248];
 
         private readonly Timer _timer;
 
@@ -82,30 +83,38 @@ namespace PLC.Sharp7.Concrete
                 {
                     int res = client.DBRead(41, 0, 248, buffer41);
 
-                    DB41.Akm = S7.GetRealAt(buffer41, 56);
-                    DB41.TesisDebi = Get.Real(buffer41, 0, 60);
-                    DB41.TesisGünlükDebi = Get.Real(buffer41, 12, 60);
-                    DB41.DesarjDebi = Get.Real(buffer41, 60, 60); //Taşkan Debisi
-                    DB41.HariciDebi = Get.Real(buffer41, 52, 60); //Çıkış Terfi Merkezi Debisi
-                    DB41.HariciDebi2 = Get.Real(buffer41, 56, 60); //2. Kademe Çıkış Debisi
-                    DB41.NumuneHiz = Get.Real(buffer41, 4);
-                    DB41.NumuneDebi = Get.Real(buffer41, 8);
-                    DB41.Ph = Get.Real(buffer41, 16);
-                    DB41.Iletkenlik = Get.Real(buffer41, 20);
-                    DB41.CozunmusOksijen = Get.Real(buffer41, 24);
-                    DB41.NumuneSicaklik = Get.Real(buffer41, 28);
-                    DB41.Koi = Get.Real(buffer41, 32);
-                    DB41.Akm = Get.Real(buffer41, 36);
-                    DB41.KabinNem = Get.Real(buffer41, 44);
-                    DB41.KabinSicaklik = Get.Real(buffer41, 40);
-                    DB41.Pompa1Hz = Get.Real(buffer41, 140);
-                    DB41.Pompa2Hz = Get.Real(buffer41, 144);
-                    DB41.UpsGirisVolt = Get.Real(buffer41, 152);
-                    DB41.UpsCikisVolt = Get.Real(buffer41, 148);
-                    DB41.UpsKapasite = Get.Real(buffer41, 156);
-                    DB41.UpsSicaklik = Get.Real(buffer41, 160);
-                    DB41.UpsYuk = Get.Real(buffer41, 164);
+                    if (res == 0)
+                    {
+                        DB41.Akm = S7.GetRealAt(buffer41, 56);
+                        DB41.TesisDebi = Get.Real(buffer41, 0, 60);
+                        DB41.TesisGünlükDebi = Get.Real(buffer41, 12, 60);
+                        DB41.DesarjDebi = Get.Real(buffer41, 60, 60);  //Taşkan Debisi
+                        DB41.HariciDebi = Get.Real(buffer41, 52, 60);  //Çıkış Terfi Merkezi Debisi
+                        DB41.HariciDebi2 = Get.Real(buffer41, 56, 60); //2. Kademe Çıkış Debisi
+                        DB41.NumuneHiz = Get.Real(buffer41, 4);
+                        DB41.NumuneDebi = Get.Real(buffer41, 8);
+                        DB41.Ph = Get.Real(buffer41, 16);
+                        DB41.Iletkenlik = Get.Real(buffer41, 20);
+                        DB41.CozunmusOksijen = Get.Real(buffer41, 24);
+                        DB41.NumuneSicaklik = Get.Real(buffer41, 28);
+                        DB41.Koi = Get.Real(buffer41, 32);
+                        DB41.Akm = Get.Real(buffer41, 36);
+                        DB41.KabinNem = Get.Real(buffer41, 44);
+                        DB41.KabinSicaklik = Get.Real(buffer41, 40);
+                        DB41.Pompa1Hz = Get.Real(buffer41, 140);
+                        DB41.Pompa2Hz = Get.Real(buffer41, 144);
+                        DB41.UpsGirisVolt = Get.Real(buffer41, 152);
+                        DB41.UpsCikisVolt = Get.Real(buffer41, 148);
+                        DB41.UpsKapasite = Get.Real(buffer41, 156);
+                        DB41.UpsSicaklik = Get.Real(buffer41, 160);
+                        DB41.UpsYuk = Get.Real(buffer41, 164);
+                    }
+                    else
+                    {
+                        Reconnect();
 
+                        //TODO
+                    }
                 };
                 _worker.RunWorkerAsync();
             }
