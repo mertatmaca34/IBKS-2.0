b6251f8e76a877b6714016f589b25461b89552ed
Mert Atmaca <mertatmaca34@gmail.com>
2023-07-21 15:53:27 +0300

    ++

---
+++ Entities/Concrete/CalibrationLimit.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class CalibrationLimit : IEntity
+    {
+        public int Id { get; set; }
+        public string Parameter { get; set; }
+        public int ZeroRef { get; set; }
+        public int ZeroTimeStamp { get; set; }
+        public int SpanRef { get; set; }
+        public int SpanTimeStamp { get; set; }
+    }
+}
