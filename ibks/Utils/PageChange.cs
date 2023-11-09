9a0198c8e5e2a6787973e902c25a50b0f6bf36ee
Mert Atmaca <mertatmaca34@gmail.com>
2023-11-09 13:53:38 +0300

    ++

--- ibks/Utils/PageChange.cs
+++ ibks/Utils/PageChange.cs
@@ -13,6 +13,20 @@
             {
                 activeForm.Size = panel.Size;
             }
+
+            if (ChildForm.Name == "SimulationPage")
+            {
+                Main.WindowState = FormWindowState.Normal;
+                Main.MaximumSize = new Size(1280, 720);
+
+                Main.MaximizeBox = false;
+            }
+            else
+            {
+                Main.MaximumSize = new Size(0, 0);
+
+                Main.MaximizeBox = true;
+            }
         }
     }
 }
