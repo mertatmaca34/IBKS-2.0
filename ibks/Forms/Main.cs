d34ca35cf0f6c0a7d186c74e46228bcc59a6c674
Mert Atmaca <mertatmaca34@gmail.com>
2023-12-22 14:47:49 +0300

    ++

--- ibks/Forms/Main.cs
+++ ibks/Forms/Main.cs
@@ -9,60 +9,44 @@ namespace ibks.Forms
 {
     public partial class Main : Form
     {
-        readonly HomePage _homePage;
-        readonly SimulationPage _simulationPage;
-        readonly ReportingPage _reportingPage;
-        readonly CalibrationPage _calibrationPage;
+        private readonly HomePage _homePage;
+        private readonly SimulationPage _simulationPage;
+        private readonly ReportingPage _reportingPage;
+        private readonly CalibrationPage _calibrationPage;
 
-        readonly IStationService _stationManager;
-        readonly IApiService _apiManager;
-        readonly ISendDataService _sendDataManager;
-        readonly ICalibrationService _calibrationManager;
-        readonly ICalibrationLimitService _calibrationLimitManager;
-        readonly IPlcService _plcManager;
-        readonly IMailServerService _mailServerManager;
-        readonly IAuthService _authManager;
-        readonly IUserService _userManager;
-        readonly IMailStatementService _mailStatementManager;
-        readonly IUserMailStatementService _userMailStatementManager;
-        readonly ILogin _login;
-        readonly ISendCalibrationController _sendCalibrationController;
-        readonly ISendDataController _sendDataController;
-        readonly ICheckStatements _checkStatements;
-        readonly IGetMissingDatesController _getMissingDatesController;
-        readonly ISampleService _sampleManager;
+        private readonly IStationService _stationManager;
+        private readonly IApiService _apiManager;
+        private readonly ICalibrationLimitService _calibrationLimitManager;
+        private readonly IPlcService _plcManager;
+        private readonly IMailServerService _mailServerManager;
+        private readonly IAuthService _authManager;
+        private readonly IUserService _userManager;
+        private readonly IMailStatementService _mailStatementManager;
+        private readonly IUserMailStatementService _userMailStatementManager;
 
         public Main(IStationService stationManager, IApiService apiManager, ISendDataService sendDataManager,
             ICalibrationService calibrationManager, ICalibrationLimitService calibrationLimitManager,
             IPlcService plcManager, IMailServerService mailServerManager, IAuthService authManager,
             IUserService userManager, IMailStatementService mailStatementManager, IUserMailStatementService userMailStatementManager,
-            ILogin login, ISendDataController sendDataController, ISendCalibrationController sendCalibrationController,
-            ICheckStatements checkStatements, IGetMissingDatesController getMissingDatesController, ISampleService sampleManager)
+            ISendDataController sendDataController, ISendCalibrationController sendCalibrationController,
+            ICheckStatements checkStatements, ISampleService sampleManager)
         {
             InitializeComponent();
 
             _apiManager = apiManager;
             _stationManager = stationManager;
             _plcManager = plcManager;
-            _sendDataManager = sendDataManager;
-            _calibrationManager = calibrationManager;
             _calibrationLimitManager = calibrationLimitManager;
             _mailServerManager = mailServerManager;
             _authManager = authManager;
             _userManager = userManager;
             _mailStatementManager = mailStatementManager;
             _userMailStatementManager = userMailStatementManager;
-            _login = login;
-            _sendDataController = sendDataController;
-            _sendCalibrationController = sendCalibrationController;
-            _checkStatements = checkStatements;
-            _getMissingDatesController = getMissingDatesController;
-            _sampleManager = sampleManager;
 
-            _homePage = new HomePage(_stationManager, _sendDataManager, _calibrationManager, _sendDataController, _checkStatements);
+            _homePage = new HomePage(_stationManager, sendDataManager, calibrationManager, sendDataController, checkStatements);
             _simulationPage = new SimulationPage();
-            _reportingPage = new ReportingPage(_sendDataManager, _calibrationManager, _sampleManager);
-            _calibrationPage = new CalibrationPage(_calibrationManager, _stationManager, _calibrationLimitManager, _apiManager, _sendCalibrationController);
+            _reportingPage = new ReportingPage(sendDataManager, calibrationManager, sampleManager);
+            _calibrationPage = new CalibrationPage(calibrationManager, _stationManager, _calibrationLimitManager, _apiManager, sendCalibrationController);
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -92,9 +76,9 @@ namespace ibks.Forms
 
         private void ButtonMailPage_Click(object sender, EventArgs e)
         {
-            var res = LoginOps.Login(_authManager);
+            var loginSuccessful = LoginOps.Login(_authManager);
 
-            if (res == true)
+            if (loginSuccessful)
             {
                 PageChange.Change(PanelContent, this, new MailPage(_mailServerManager, _authManager, _userManager, _mailStatementManager, _userMailStatementManager));
                 ButtonImageExtensions.Replace(TableLayoutPanelLeftBar, ButtonMailPage);
@@ -111,11 +95,9 @@ namespace ibks.Forms
         {
             var res = LoginOps.Login(_authManager);
 
-            if (res == true)
-            {
-                PageChange.Change(PanelContent, this, new SettingsPage(_calibrationLimitManager, _apiManager, _stationManager, _plcManager));
-                ButtonImageExtensions.Replace(TableLayoutPanelLeftBar, ButtonSettingPage);
-            }
+            if (res != true) return;
+            PageChange.Change(PanelContent, this, new SettingsPage(_calibrationLimitManager, _apiManager, _stationManager, _plcManager));
+            ButtonImageExtensions.Replace(TableLayoutPanelLeftBar, ButtonSettingPage);
         }
 
         private void Main_SizeChanged(object sender, EventArgs e)
